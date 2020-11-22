aabbfb0a16c94689afc6627ad4b6cc4d26cb73e9
RevolNoom <revolnoom7801@gmail.com>
2020-11-22 20:58:15 +0700

    Final commit

--- GitPush.sh
+++ GitPush.sh
@@ -1,4 +1,4 @@
 git add *
 git rm -f assets/src/ninja.so assets/src/*.o 
 git commit --allow-empty-message
-git push -u "https://github.com/RevolNoom/Ninja"
+git push  "https://github.com/RevolNoom/Ninja"
